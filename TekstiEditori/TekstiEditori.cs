5e2cb14d57538c816cad31a18b3dc3162d05f51a
Lassi Jääskeläinen <lassijaa@gmail.com>
2025-10-01 13:01:56 +0300

    Committing an empty Rider project using the ConsoleMain project template

---
+++ TekstiEditori/TekstiEditori.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+/// @author lassi
+/// @version 01.10.2025
+/// <summary>
+/// 
+/// </summary>
+public class TekstiEditori
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static void Main()
+    {
+    }
+}
