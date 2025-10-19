8befe77381437b9ad41ef0a9ba1de806b7c381bf
Lassi Jääskeläinen <lassi@lassijaaskelainen.fi>
2025-10-19 20:13:54 +0300

    Tuotettu alustavaa koodia tekstin editointiin ja tallentamiseen liittyen
    - On nyt olemassa jonkinlainen merkkijonobufferi jota pystyy jokseenkin
      mielekkäästi kirjoittamaan ja tämä tallentuu lopuksi tiedostoon.
    - Tähän mennessä kyse kuitenkin ollut enemmän koodin hahmottelusta
      loppujen lopuksi kuin siitä että mikään toiminnallisuus
      olisi lopullisesti valmistunut.

--- TekstiEditori/TekstiEditori.cs
+++ TekstiEditori/TekstiEditori.cs
@@ -1,19 +1,69 @@
 using System;
+using System.IO;
 using System.Text;
 using System.Linq;
 using System.Collections.Generic;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Threading;
 
 /// @author lassi
 /// @version 01.10.2025
 /// <summary>
-/// 
+/// Ohjelman pääluokka
 /// </summary>
 public class TekstiEditori
 {
+    private static ConsoleKeyInfo syote;
+    private static StringBuilder bufferi;
+    private static readonly int bufferinKoko = 5000;
+    private static readonly string fileName = "./tiedosto.txt";
     /// <summary>
-    /// 
+    /// Ohjelman päämetodi
     /// </summary>
     public static void Main()
     {
+        Console.TreatControlCAsInput = true;
+        Console.WriteLine("Kirjoita teksi, paina DEL lopettaksesi:");
+        bufferi = new StringBuilder("", bufferinKoko);
+        do
+        {
+            syote = Console.ReadKey(false);
+            bufferi.Append(MerkinTunnistus(syote.Key, syote.KeyChar));
+        } while (syote.Key != ConsoleKey.Delete);
+        Console.WriteLine("\n" + bufferi.ToString());
+        Console.WriteLine($"Bufferia käytetty: {bufferi.Length} / {bufferinKoko}");
+        File.WriteAllText(fileName, bufferi.ToString());
+        Console.WriteLine($"Kirjoitettu tuloste tiedostoon {fileName}");
+    }
+
+    /// <summary>
+    /// Tarkoitettu tiettyjen erikoismerkkien läpikäymiseen.
+    /// </summary>
+    /// <param name="nappain"></param>
+    /// <param name="merkki"></param>
+    /// <returns></returns>
+    public static char MerkinTunnistus(ConsoleKey nappain, char merkki)
+    {
+        switch (nappain)
+        {
+            case ConsoleKey.Enter:
+                return '\n';
+            case ConsoleKey.Backspace:
+                PoistaSyotetteesta(2);
+                return '\0';
+            default:
+                return merkki;
+        }
+    }
+
+    /// <summary>
+    /// Tällä voi poistaa merkkejä syötteestä, esimerkiksi kun ollaan käytetty backspacea
+    /// syötettä annettaessa.
+    /// </summary>
+    /// <param name="syvyys"></param>
+    public static void PoistaSyotetteesta(int syvyys)
+    {
+        bufferi.Remove(bufferi.Length - syvyys, syvyys);
     }
 }
